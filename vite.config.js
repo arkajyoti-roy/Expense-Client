5ae4224f6d0c272bf5ea7d3936987edbec1c08bf
Arkajyoti Roy <163657852+arkajyoti-roy@users.noreply.github.com>
2025-06-20 17:51:26 +0530

    update 2 files and delete 1 file

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
-  plugins: [react(),
-     tailwindcss(),
-  ],
-})
+  plugins: [react(), tailwindcss()],
+  server: {
+    host: '0.0.0.0',                  // Allows access from external servers
+    port: parseInt(process.env.PORT) || 5173  // Uses env PORT or defaults to 5173
+  }
+});
