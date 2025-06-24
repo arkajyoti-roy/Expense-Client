8d8c4818e2cd99e9c030f9f5d96de387afed1d39
Arkajyoti Roy <163657852+arkajyoti-roy@users.noreply.github.com>
2025-06-24 13:33:38 +0530

    update url.js

--- src/Components/url.js
+++ src/Components/url.js
@@ -1,2 +1,3 @@
-const BASE_URL = import.meta.env.VITE_API_URL?.trim() || "http://localhost:8080";
+const BASE_URL = "https://expense-server-neoq.onrender.com";
+// const BASE_URL = import.meta.env.VITE_API_URL?.trim() || "http://localhost:8080";
 export default BASE_URL;
