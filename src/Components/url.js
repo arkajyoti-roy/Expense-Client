d09122c2acf31e00d3ee364ad9c370c5bc4971e1
Arkajyoti Roy <163657852+arkajyoti-roy@users.noreply.github.com>
2025-06-24 13:32:15 +0530

    update url.js

--- src/Components/url.js
+++ src/Components/url.js
@@ -1,6 +1,2 @@
-const BASE_URL =
-  import.meta.env.VITE_API_URL?.trim() !== ""
-    ? import.meta.env.VITE_API_URL
-    : "http://localhost:8080";
-
+const BASE_URL = import.meta.env.VITE_API_URL?.trim() || "http://localhost:8080";
 export default BASE_URL;
